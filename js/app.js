4a839958295911948d9ff19ddf3523a1a17ad646
afzalhossain00 <afzalhossain.workspace@gmail.com>
2022-08-23 04:44:41 +0600

    player selection added

--- js/app.js
+++ js/app.js
@@ -8,6 +8,27 @@ function addToClick(player) {
         names: playerName
     };
     playerNamesArray.push(playerNameList);
-    document.getElementById('total-player-added').innerText = playerNamesArray.length;
+    playerListDisplay(playerNamesArray);
+    player.disabled = true;
+    player.style.backgroundColor = 'gray';
+}
 
+// display in table
+function playerListDisplay(slectedPlayer) {
+    const slectedPlayerList = document.getElementById('add-players');
+    slectedPlayerList.innerHTML = '';
+
+    for (let i = 0; i < slectedPlayer.length; i++) {
+        const displayName = playerNamesArray[i].names
+        const tr = document.createElement('tr');
+        if (i > 4) {
+            return alert('You can not select more than five player');
+        }
+        tr.innerHTML = `
+                <th>${i + 1}</th>
+                <td>${displayName}</td>
+                `;
+        slectedPlayerList.appendChild(tr);
+    }
+    const selectPlayerNumber = document.getElementById('total-player-added').innerText = playerNamesArray.length;
 }
